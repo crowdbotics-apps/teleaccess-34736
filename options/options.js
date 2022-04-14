528951081604fab6e2d7a0f347b5d508a7e0bb03
Crowdbotics <team@crowdbotics.com>
2022-04-14 03:34:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "teleaccess_34736",
+  url: "https://teleaccess_34736.botics.co",
+  api: "https://teleaccess_34736.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
